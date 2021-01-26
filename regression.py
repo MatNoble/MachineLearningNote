42c1ccf072c63c22fffdb6959af584f4ba9f5e9b
MatNoble <z-matrix@qq.com>
2021-01-26 11:34:11 +0800

    update regression

--- regression.py
+++ regression.py
@@ -3,94 +3,111 @@ from numpy import linalg as LA
 import matplotlib.pyplot as plt
 
 class regression:
-       
-    def train(self, x, y, X, n=1, w=np.mat(np.zeros(3)), lr=0.2, lam=0, Iter=500):
-        y_hat = X * w.T
+    def __init__(self, y, X, w=np.zeros((3, 1)), n=1, lr=0.2, lam=0, MAX_Iter=500, stop_condition=1e-4):
+        # axis
+        self.x, self.y = X[:,-2], y
+        # matrix
+        self.X, self.mu, self.std = self.normalize(X)
+        # polynomial of degree n
+        self.n = n
+        # weights
+        self.w = w
+        # learning rate
+        self.lr = lr
+        # lambda
+        self.lam = lam
+        # The maximum number of iterations
+        self.MAX_Iter = MAX_Iter
+        # stop condition
+        self.stop_condition = stop_condition
+
+    def normalize(self, data):
+        mu = std = []
+        for i in range(data.shape[1]-1):
+            data[:,i] = (data[:,i] - np.mean(data[:,i]))/np.std(data[:, i])
+            mu.append(np.mean(data[:,i]))
+            std.append(np.std(data[:, i]))
+        return data, mu, std
         
+    def train(self):
+        t, w = 1, self.w
+        y_hat = self.X @ w
         plt.ion()
-        adaGrad = np.mat(np.zeros(n+1))
-        for t in range(Iter):
-            # Adagrad
-            grad = self.Grad(lam, w.copy(), x, y, X, y_hat)
-            temp = np.vstack((adaGrad, grad))
-            adaGrad = LA.norm(temp, axis=0)
-            
+        while True:
             # updata w
             wn = w
-            w = wn - lr*grad/adaGrad
-            # w = wn - lr*grad
+            w = wn - self.lr * self.Grad(y_hat, w.copy())
             
             # compute loss
-            y_hat = X * w.T
-            loss = self.MSELoss(lam, w, y, y_hat)
+            y_hat = self.X @ w
+            loss = self.MSELoss(y_hat, w)
             
             # Visualization
-            if t % 5 == 0: self.visual(x, y, y_hat, loss)
+            if t % 5 == 0: self.visual(y_hat, loss)
            
             # end condition
-            if LA.norm(w-wn) < 1E-4 or t == Iter-1:
-                print('\n iteration number: ', t+1)
+            if LA.norm(w-wn) < self.stop_condition or t > self.MAX_Iter:
+                print('\n iteration number: ', t)
                 break
+            t += 1
         # out put
-        print('w = \n', w.T)
+        print('w = \n', w)
         print('++++++++++++++++++++++++++++')
         plt.ioff()
+        return w, self.mu, self.std
 
-    def MSELoss(self, lam, w, y, y_hat):
-        return (y-y_hat).T*(y-y_hat)/2.0/len(y) + lam*self.Regularization(w)/2.0/len(y)
+    def MSELoss(self, y_hat, w):
+        m = 0.5/self.y.shape[0]
+        return m*((self.y-y_hat).T @ (self.y-y_hat) + self.lam * self.Regularization(w))
 
     def Regularization(self, w, k=1):
         if k==1: # L2
-            temp = np.asarray(w)[0][:-1]
-            return np.power(LA.norm(temp), 2)
+            return w.T @ w
         # elif k==2: # L1
     
-    def Grad(self, lam, w, x, y, X, y_hat):
-        w[0, -1] = 0
-        return (y_hat-y).T * X/len(y) + lam*w/len(y)
+    def Grad(self, y_hat, w):
+        w[-1] = 0
+        m = 1.0/self.y.shape[0]
+        return m*(self.X.T @ (y_hat-self.y) + self.lam * w)
     
-    # def stochasticGrad(self, x, y, X, y_hat):
-    #     i = np.random.randint(0, len(x)-1)
-    #     return (y_hat[i]-y[i]) * X[i,:]
-
-    def visual(self, x, y, y_hat, loss):
+    def visual(self, y_hat, loss):
         # plot and show learning process
         plt.cla()
-        plt.scatter(np.array(x), np.array(y))
-        plt.plot(np.array(x), np.array(y_hat), 'r-', lw=4)
-        plt.text(0.4, min(y), 'Loss=%.5f' %loss, fontdict={'size': 16, 'color': 'red'})
+        plt.scatter(self.x, self.y)
+        plt.plot(self.x, y_hat, 'r-', lw=4)
+        plt.text(-1.5, 17.5, 'Loss=%.5f' %loss, fontdict={'size': 18, 'color': 'red'})
         plt.xticks(fontsize=14)
         plt.yticks(fontsize=14)
         plt.grid(True)
         plt.pause(0.1)
 
-# =====================================
-# Main
-# =====================================
-mat = regression()
-# fake data
-x = np.linspace(-1, 1, 200)
-y = np.exp(x*x*x)*np.power(x, 3) + 0.4*np.random.randn(len(x))
-
-# vector to matrix
-x, y = np.mat(x), np.mat(y)
-x, y = x.T, y.T
-
-# Visualization
-plt.figure(figsize=(15, 10))
-mngr = plt.get_current_fig_manager()
-mngr.window.wm_geometry("+50+310")
-plt.suptitle('Regression', fontsize=20)
-
-for n in range(1, 5):
-    X = np.mat(np.ones(len(x))).T
-    for i in range(n):
-        temp = np.mat(np.power(x, i+1))
-        X = np.hstack((temp, X))
-    # w = np.mat(np.random.randn(n+1))
-    plt.subplot(2,2,n)
-    w = np.mat(np.zeros(n+1))
-    mat.train(x, y, X, n=n, w=w, lr=0.4, lam=0.1)
-
-print("Bingo")
-plt.show()
+if __name__ == "__main__":
+    # =====================================
+    # Main
+    # =====================================
+    # fake data
+    x = np.linspace(-2, 1, 200)
+    y = np.power(x, 2) + 0.2*np.random.randn(len(x))
+    # y = np.exp(x/2.0)*np.power(x, 3) + 0.4*np.random.randn(len(x))
+    x = np.reshape(x, (x.shape[0], 1))
+    y = np.reshape(y, (y.shape[0], 1))
+    
+    # Visualization
+    plt.figure(figsize=(15, 10))
+    mngr = plt.get_current_fig_manager()
+    mngr.window.wm_geometry("+50+310")
+    plt.suptitle('Regression', fontsize=20)
+    
+    for n in range(1, 5):
+        plt.subplot(2,2,n)
+        X = np.ones((x.shape[0], 1))
+        for i in range(n):
+            temp = np.power(x, i+1)
+            X = np.hstack((temp, X))
+        w = np.random.randn(X.shape[1], 1)
+        # w = np.zeros((X.shape[1], 1))
+        mat = regression(y, X, w=w, n=n, lr=0.01, lam=100)
+        mat.train()
+    
+    print("Bingo")
+    plt.show()
